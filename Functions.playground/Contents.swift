0c20441d4792606cfeefdae57b78f2d06a69a6ca
Bhuwan Bansal <bhuwan.bansal@outlook.com>
2023-10-31 14:51:55 +1100

    SiftUI Crash Course

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -28,5 +28,19 @@ func customMinus(_ lhs: Int, _ rhs: Int) -> Int {
 
 let customSubtracted = customMinus(20, 10)
 
+let added = customAdd (value1: 20, value2: 30)
+
+// Attribute is used to indicate that it's acceptable to ignore the result
+// returned by a funciton. In situations where you have a function that performs an operation and retusn a result, but you may not need that result right away. In such cases "@discardableResult" attribute allows you to call the function and ignore its return value without triggering a warning from a Swift compiler
+@discardableResult
+func myCustomAdd (_ lhs: Int, _ rhs: Int) -> Int {
+    return lhs + rhs
+}
+
+myCustomAdd(20, 30)
+// this can be handy where you want to provide flexibility to the caller
+// its not about adding two values specifically, it's about indicating that the result of a funciton
+
+
 
 
---
+++ MyApp/MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Bhuwan bansal on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
